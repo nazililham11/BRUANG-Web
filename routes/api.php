78709a535c8710c0a5cab40f9159d9bcc4c98eba
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2019-12-30 12:37:38 +0700

    Add Booking Route

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('booking', 'Api\BookingController@getFreeRoom')->name('getRoom');
+
 Route::post('login', 'Api\AuthController@login');
