bf5fcde329ca2692e9f682fb0f12cb197ac00390
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2019-12-30 14:17:26 +0700

    Add Details and Logout Method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,12 +10,25 @@ class AuthController extends Controller
 {
     public function login(){ 
 		if(Auth::attempt(['user_id' => request('user_id'), 'password' => request('password')])){ 
+			
 		   	$user = Auth::user(); 
-		   	$success['token'] =  $user->createToken('AppName')-> accessToken; 
+		   	$success['token'] =  $user->createToken('AppName')->accessToken; 
 
 		    return response()->json(['success' => $success], 200); 
 	  	} else{ 
 		   	return response()->json(['error'=>'Unauthorised'], 401); 
 	   	} 
 	}
+
+	public function details() 
+    { 
+        $user = Auth::user(); 
+        return response()->json(['success' => $user], 200); 
+    } 
+
+    public function logout() 
+    { 
+        auth()->guard('api')->logout();
+        return response()->json(['success' => 'success'], 200); 
+    } 
 }
