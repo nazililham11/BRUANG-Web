3b0846da7eedbf8bbaeafab8ded9d8434e8e3add
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-01-01 14:36:41 +0700

    Change Json Response format

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\Auth; 
@@ -9,14 +10,25 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     public function login(){ 
+    	$validator = Validator::make(request()->all(), [
+            "user_id" 	=> "required|exists:users,user_id",
+            "password"	=> "required|min:4",
+        ]);
+
+    	if ($validator->fails()) {
+            return response()->json(['status' => 'error', 'message' => $validator->errors()->first()]);        
+        }
 		if(Auth::attempt(['user_id' => request('user_id'), 'password' => request('password')])){ 
 			
 		   	$user = Auth::user(); 
-		   	$success['token'] =  $user->createToken('AppName')->accessToken; 
+		   	$success['status'] = 'success';
+		   	$success['message'] = 'Login Success';
+		   	$success['user'] = $user->only('name', 'user_id');
+		   	$success['user']['token'] =  $user->createToken('AppName')->accessToken; 
 
-		    return response()->json(['success' => $success], 200); 
+		    return response()->json($success, 200); 
 	  	} else{ 
-		   	return response()->json(['error'=>'Unauthorised'], 401); 
+		   	return response()->json(['status' => 'error', 'message' => 'Unauthorised'], 401); 
 	   	} 
 	}
 
--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -3,72 +3,55 @@
 namespace App\Http\Controllers\Api;
 
 use App\Room;
+use Validator;
 use App\Booking;
 use App\ClassSession;
-use Illuminate\Http\Request; 
 use App\ClassSchedule;
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\DB;
-use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth; 
 
 class BookingController extends Controller
 {
 
-    public function getFreeRoom(Request $request)
+    public function index(Request $request)
     {
+        $user = Auth::user(); 
+        // $bookings = DB::table('bookings')->where('user_id', $user->id)->get();       
+        $bookings = Booking::where('user_id', $user->id)->with(['class_schedule.room', 'class_schedule.class_session'])->get();
+        $success['status'] = 'success';
+        $success['message'] = 'success';
+        $success['bookings'] = $bookings;
 
-        $validatedData = Validator::make(request()->all(), [
-            "dates"    => "required|array|min:1",
-            "dates.*"  => "required|date|after_or_equal:today",
-        ]);
-
-        if ($validatedData->fails()) {
-            return response()->json(['error' => $validatedData->errors()]);        
-        }
-
-        //Get Booking Item
-        $bookings = DB::table('bookings')->where('day', 5)->get('class_schedule_id');
-        
-        //Get Course Schedule Item
-        $courses = DB::table('course_schedules')
-                        ->join('class_schedules', 'class_schedules.id', '=', 'course_schedules.class_schedule_id')
-                        ->where('class_schedules.day', 5)
-                        ->get('class_schedules.id')->toArray();
-
-        //Convert to Array
-        $bookingsRecord = array();
-        $courseItems = array();
-
-        foreach ($bookings as $booking) {
-             array_push($bookingsRecord, $booking->class_schedule_id);
-        }
-        foreach ($courses as $val) {
-            array_push($courseItems, $val->id);
-        }
-
-        // dd($bookingsRecord);
-        // dd($courseItems);
-
-        //Get Class Schedule Where not in booking and Course Schedule
-        $datas = DB::table('class_schedules')
-                ->where('class_schedules.day', 5)
-                ->whereNotIn('class_schedules.id', $courseItems)
-                ->whereNotIn('class_schedules.id', $bookingsRecord)
-                ->join('rooms', 'rooms.id', '=', 'class_schedules.room_id')
-                ->join('class_sessions', 'class_sessions.id', '=', 'class_schedules.class_session_id')
-                ->get(['*', 'class_schedules.id']);
-
-        //Convert datas to JSON
-    	$datas = json_decode(json_encode($datas), true);
-
-        //return datas
-        return response()->json(['success' => $datas], 200); 
-        
+        return response()->json($success, 200); 
     }
 
-    public function getBookings(Request $request)
+    public function store(Request $request)
     {
-    	
+
+        $validator = Validator::make(request()->all(), [
+            "class_schedule_id" => "required|exists:class_schedules,id",
+            "title"             => "required|max:100",
+            "date"              => "required|date|after_or_equal:today",
+            "day"               => "required|integer|between:1,6",
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()]);        
+        }
+
+        $user = Auth::user(); 
+
+        //Get Booking Item
+        $bookings = DB::table('bookings')->where('user_id', $user->id)->get();
+
+
+        $success['status'] = 'success';
+        $success['message'] = 'Booking Success';
+
+        return response()->json($success, 200); 
+        
     }
 
     
