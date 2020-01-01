6ab009d6c529ce86825067044e2043b38cbc78a1
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-01-01 14:38:28 +0700

    Rename Routes and Add 404 Routes

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider within a group which   
+| is assigned the "api" middleware group. Enjoy building your API!  
 |
 */
 
@@ -18,14 +18,24 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
+Route::group(['prefix' => 'v1'], function(){
+	
+	Route::post('login', 'Api\AuthController@login');
 
-Route::post('login', 'Api\AuthController@login');
+	Route::group(['middleware' => 'auth:api'], function(){
+	   	Route::post('logout', 'Api\AuthController@logout')->name('api.logout');
+	   	Route::post('details', 'Api\AuthController@details')->name('api.details');
 
-Route::group(['middleware' => 'auth:api'], function(){
-   	Route::post('logout', 'Api\AuthController@logout')->name('api.logout');
-   	Route::post('details', 'Api\AuthController@details')->name('api.details');
+		Route::post('rooms', 'Api\RoomController@index')->name('api.getRooms');
+		Route::get('bookings', 'Api\BookingController@index')->name('api.getBookings');
+		Route::post('createbookin', 'Api\BookingController@store')->name('api.create.booking');
 
-	Route::post('rooms', 'Api\RoomController@index')->name('api.getRooms');
-	Route::post('bookings', 'Api\BookingController@index')->name('api.getBookings');
-	Route::post('createbookin', 'Api\BookingController@store')->name('api.create.booking');
+		Route::any('{any}', function() {
+		    return response()->json(['error' => '404 Not Found'], 200); 
+		});
+	});
+});
+
+Route::any('{any}', function() {
+    return response()->json(['error' => '404 Not Found'], 200); 
 });
